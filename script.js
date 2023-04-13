c854013985b0279751b9e467c5efbb48a4b3610b
¨YaidelLuis¨ <yaidelaraujo@gmail.com>
2023-04-13 12:01:01 -0400

    terminada sesion de habilidades

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("htmlcss");
-        habilidades[1].classList.add("javascript");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[0].classList.add("flutter");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("javascript");
+        habilidades[3].classList.add("booststrap");
+        habilidades[4].classList.add("pythondjango");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
